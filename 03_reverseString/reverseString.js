2d27a84a90c588e520b91e1b5fc72e3f781b4d05
logicalmind <ilikelogic@skiff.com>
2022-08-23 16:45:12 -0700

    Solution to removeFromArray.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let revStr = '';
+    for (i = 0; i < str.length; i++) {
+    revStr += str[(str.length - 1) - i];
+    }
+    return revStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...arrElements) {
+    let a;
+for (let i = 0; i < arrElements.length; i++) { 
+        a = arrElements[i];
+    for (let i = 0; i < arr.length; i++) {
+            if (a === arr[i]) {
+        arr.splice(i, 1);
+        }
+    }
+  }
+return arr;
 };
 
 // Do not edit below this line
