f0319098bd6c8dd2dab8619ef60779efd8b9153b
logicalmind <ilikelogic@skiff.com>
2022-08-23 19:07:50 -0700

    Indent for better readability

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,19 +1,19 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
     if (num1 < 0 || num2 < 0 || Number.isInteger(num2) === false || Number.isInteger(num1) === false) {
-    return 'ERROR';
+        return 'ERROR';
     } else if (num1 < num2) {
         while (num1 <= num2) {
             sum += num1;
             ++num1;
         }
-        return sum;
+            return sum;
     } else if (num1 > num2) {
         while (num1 >= num2) {
             sum += num2;
             ++num2;
         } 
-        return sum;
+            return sum;
     }
 };
 
