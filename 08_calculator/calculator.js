6ad405cff3bce18d881f4876215c11ab95af93f8
logicalmind <ilikelogic@skiff.com>
2022-08-24 11:15:53 -0700

    Solution for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,50 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let i = 0;
+  let sum = 0;
+  if (arr.length === i) { 
+    return sum
+  }
+  while (i < arr.length) {
+    sum += arr[i]
+    i++;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let i = 0;
+  let product = 1;
+  if (arr.length === i) {
+    return i;
+  } else {
+      while (i < arr.length) {
+        product *= arr[i];
+        i++
+      }
+      return product;
+  }
+    }
 
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let product = 1;
+  while (num > 0) {
+  product *= num;
+  num--;
+  }
+  return product;
 };
 
 // Do not edit below this line
