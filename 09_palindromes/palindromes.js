8c38372dfecdf47352d4cda07e68c66e9eb2553e
logicalmind <ilikelogic@skiff.com>
2022-08-24 13:32:15 -0700

    Solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (words) {
+    words = words.toLowerCase();
+    words = words.match(/[a-z]/g);
+    let backwards = Array.from(words);
+    backwards = backwards.reverse();
+    if (words.toString() === backwards.toString()) {
+    		return true
+    } else {
+    		return false
+    }
+ };
 
 // Do not edit below this line
 module.exports = palindromes;
