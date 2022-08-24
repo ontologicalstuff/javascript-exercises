cb70e3d54d60af160bc80236b50e154c097151fe
logicalmind <ilikelogic@skiff.com>
2022-08-24 10:06:14 -0700

    Solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  let ctemp = (ftemp -32) * (5/9);
+  if ( !(Number.isInteger(ctemp)) && !(Number.isNaN(ctemp)) ) {
+    return Math.round(ctemp * 10) / 10;
+  } else return ctemp;
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+ let ftemp = ctemp * (9/5) + 32;
+ if ( !(Number.isInteger(ftemp)) && !(Number.isNaN(ftemp)) ) {
+  return Math.round(ftemp * 10) / 10;
+ } else return ftemp;
 };
 
 // Do not edit below this line
