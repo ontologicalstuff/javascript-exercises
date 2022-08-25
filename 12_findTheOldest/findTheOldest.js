2ceb68f8a726dc973b7527bb2721d2a49da3ed0c
logicalmind <ilikelogic@skiff.com>
2022-08-25 13:26:52 -0700

    Initial solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let arrOfArr = [];
+    let ages = []
+    let date = new Date();
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+        arrOfArr.push( Object.values(arr[i]) );
+    }
+    for (let i = 0; i < arrOfArr.length; i++) {
+        if (arrOfArr[i].length > 2) {
+          ages.push(arrOfArr[i][2] - arrOfArr[i][1]);
+    }   else if (arrOfArr[i].length === 2) {
+          ages.push(date.getFullYear() - arrOfArr[i][1]);
+    }
+    }
+    let maxAge = Math.max(...ages);
+    for (let i = 0; i < ages.length; i++) {
+    if (maxAge === ages[i]) {
+      index = i;
+    }
+}
+    return arr[index];
 };
 
 // Do not edit below this line
