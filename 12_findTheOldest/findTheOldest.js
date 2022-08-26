eb17c4acb9444a08360c75ece5d674c1e911db30
logicalmind <ilikelogic@skiff.com>
2022-08-25 18:38:44 -0700

    Refactor initial solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,16 +1,37 @@
+/* First refactored solution */
 const findTheOldest = function(arr) {
-    let arrOfArr = [];
+    let ages = [];
+    let presentYear = new Date().getFullYear();
+	ages = Array.from(arr, x => x.yearOfDeath - x.yearOfBirth || 
+        presentYear - x.yearOfBirth);
+    let maxAge = Math.max(...ages);
+    for (let i = 0; i < arr.length; i++) {
+ 	if (maxAge === arr[i].yearOfDeath - arr[i].yearOfBirth || 
+        maxAge === presentYear - arr[i].yearOfBirth) {
+    	return arr[i]
+    }
+ }
+};
+
+
+
+
+
+
+/* First solution 
+const findTheOldest = function(arr) {
+    let objToArr = [];
     let ages = []
     let date = new Date();
     let index = 0;
     for (let i = 0; i < arr.length; i++) {
-        arrOfArr.push( Object.values(arr[i]) );
+        objToArr.push( Object.values(arr[i]) );
     }
-    for (let i = 0; i < arrOfArr.length; i++) {
-        if (arrOfArr[i].length > 2) {
-          ages.push(arrOfArr[i][2] - arrOfArr[i][1]);
-    }   else if (arrOfArr[i].length === 2) {
-          ages.push(date.getFullYear() - arrOfArr[i][1]);
+    for (let i = 0; i < objToArr.length; i++) {
+        if (objToArr[i].length > 2) {
+          ages.push(objToArr[i][2] - objToArr[i][1]);
+    }   else if (objToArr[i].length === 2) {
+          ages.push(date.getFullYear() - objToArr[i][1]);
     }
     }
     let maxAge = Math.max(...ages);
@@ -18,9 +39,11 @@ const findTheOldest = function(arr) {
     if (maxAge === ages[i]) {
       index = i;
     }
+
 }
     return arr[index];
-};
+}; */
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
