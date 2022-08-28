4a0cac02a908e829b617f3920b35379dfa157f76
logicalmind <ilikelogic@skiff.com>
2022-08-27 21:01:37 -0700

    Refactor solution

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,19 @@
-const reverseString = function(str) {
+/* Refactored solution 8.27.2022 */
+
+function reverseString(str) {
+    str = Array.from(str);
+    str.reverse();
+    return str.join('');
+  }
+
+
+/* const reverseString = function(str) {
     let revStr = '';
     for (i = 0; i < str.length; i++) {
     revStr += str[(str.length - 1) - i];
     }
     return revStr;
-};
+}; */
 
 // Do not edit below this line
 module.exports = reverseString;
