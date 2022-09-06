60a8af0ba3205401a51feb7297a9e56960a573e2
logicalmind <ilikelogic@skiff.com>
2022-09-06 12:43:56 -0700

    Remove extraneous semicolon

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,5 @@
 const palindromes = function (words) {
-    words = Array.from(words.toLowerCase()); // converts initial string to an array of lCase letters
+    words = Array.from(words.toLowerCase()) // converts initial string to an array of lCase letters
     .filter(word => word.match(/[a-z]/g)); // converts the array into an array of only a-z characters
     let backwards = words.join(''); // sets backwards to a string, stripping all the commas from the array
     backwards = Array.from(backwards);
