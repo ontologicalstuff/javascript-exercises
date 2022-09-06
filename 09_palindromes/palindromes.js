9a3196e7f780d9869b490801ad0937bd18a717b9
logicalmind <ilikelogic@skiff.com>
2022-09-06 13:14:24 -0700

    Remove comments

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (words) {
-    words = Array.from(words.toLowerCase()) // converts initial string to an array of lCase letters
-    .filter(word => word.match(/[a-z]/g)); // converts the array into an array of only a-z characters
-    let backwards = words.join(''); // sets backwards to a string, stripping all the commas from the array
+    words = Array.from(words.toLowerCase()) 
+    .filter(word => word.match(/[a-z]/g)); 
+    let backwards = words.join(''); 
     return words.join('') === Array.from(backwards).reverse().join('');
 }
     
