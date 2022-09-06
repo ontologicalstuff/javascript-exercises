af9b92fe22b6736eeab626271dd102d494c73af7
logicalmind <ilikelogic@skiff.com>
2022-09-06 15:51:23 -0700

    Remove extraneous indentations

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -3,12 +3,12 @@ const fibonacci = function(fibNum) {
     let seqNum = 0;
 if (fibNum == 1 || fibNum == 2) return 1;
 if (fibNum < 0) return "OOPS";
-		fibNum -=1;
+	fibNum -=1;
 for (let i = 1; i < fibNum; i++) {
      seqNum = (arr[i] + arr[i -1]);
      arr.push(seqNum);
      }
-		 return seqNum;
+	return seqNum;
 };
 
 // Do not edit below this line
