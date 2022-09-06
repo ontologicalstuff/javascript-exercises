ddc5fad1eec916b8306cd8f30cdbe9fd03e7aa96
logicalmind <ilikelogic@skiff.com>
2022-09-06 13:35:41 -0700

    Refactor solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,5 @@
 const leapYears = function(year) {
-    if (year % 400 === 0 || year % 4 === 0 && !(year % 100 === 0)) {
-        return true;
-    } else return false;
+    return (year % 400 === 0 || year % 4 === 0 && !(year % 100 === 0))
 };
 
 // Do not edit below this line
