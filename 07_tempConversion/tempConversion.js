2117b508c01a29420449c4ed3bfe982ab8001d36
logicalmind <ilikelogic@skiff.com>
2022-09-06 14:46:19 -0700

    Refactor solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,11 @@
 const ftoc = function(ftemp) {
   let ctemp = (ftemp -32) * (5/9);
-  if ( !(Number.isInteger(ctemp)) && !(Number.isNaN(ctemp)) ) {
-    return Math.round(ctemp * 10) / 10;
-  } else return ctemp;
+  return Math.round(ctemp * 10) / 10;
 };
 
 const ctof = function(ctemp) {
- let ftemp = ctemp * (9/5) + 32;
- if ( !(Number.isInteger(ftemp)) && !(Number.isNaN(ftemp)) ) {
+  let ftemp = ctemp * (9/5) + 32;
   return Math.round(ftemp * 10) / 10;
- } else return ftemp;
 };
 
 // Do not edit below this line
