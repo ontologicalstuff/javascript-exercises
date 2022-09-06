a7290839e2c6589dc7e2c43b6102abd7629f83e7
logicalmind <ilikelogic@skiff.com>
2022-09-06 13:06:33 -0700

    Refactor solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,9 +2,7 @@ const palindromes = function (words) {
     words = Array.from(words.toLowerCase()) // converts initial string to an array of lCase letters
     .filter(word => word.match(/[a-z]/g)); // converts the array into an array of only a-z characters
     let backwards = words.join(''); // sets backwards to a string, stripping all the commas from the array
-    backwards = Array.from(backwards);
-    backwards = backwards.reverse().join(''); // reverses the array, then converts it to a string stripped of commas
-    return words.join('') === backwards;
+    return words.join('') === Array.from(backwards).reverse().join('');
 }
     
 
