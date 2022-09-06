4d5f6bcb0c21065513b86d8a363ae5691cc69829
logicalmind <ilikelogic@skiff.com>
2022-09-06 10:33:09 -0700

    Refactor solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -8,34 +8,17 @@ const subtract = function(num1, num2) {
 };
 
 const sum = function(arr) {
-	let i = 0;
-  let sum = 0;
-  if (arr.length === i) { 
-    return sum;
-  }
-  while (i < arr.length) {
-    sum += arr[i];
-    i++;
-  }
-  return sum;
-};
+  return arr.reduce((pValue, cValue) => pValue + cValue,
+  0
+)};
 
 const multiply = function(arr) {
-  let i = 0;
-  let product = 1;
-  if (arr.length === i) {
-    return i;
-  } else {
-      while (i < arr.length) {
-        product *= arr[i];
-        i++;
-      }
-      return product;
-  }
-    }
+  return arr.reduce( (pValue, cValue) => pValue * cValue,
+  1
+)};
 
-const power = function(num1, num2) {
-	return num1 ** num2;
+const power = function(num, exp) {
+	return num ** exp;
 };
 
 const factorial = function(num) {
