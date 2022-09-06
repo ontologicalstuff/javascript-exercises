a09940a89440bf85d681cf32f4432cd469724196
logicalmind <ilikelogic@skiff.com>
2022-09-06 14:04:42 -0700

    Refactor solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,6 @@
 const removeFromArray = function(arr, ...arrElements) {
-    let arrElement;
-for (let i = 0; i < arrElements.length; i++) { 
-        arrElement = arrElements[i];
-    for (let i = 0; i < arr.length; i++) {
-            if (arrElement === arr[i]) {
-        arr.splice(i, 1);
-        }
-    }
+    return arr.filter(element => arrElements.indexOf(element) < 0)
   }
-return arr;
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
