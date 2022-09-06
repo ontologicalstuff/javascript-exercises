6a53ab76c5b522d852608fac1048e84ce178385e
logicalmind <ilikelogic@skiff.com>
2022-09-06 12:43:03 -0700

    Add missing semicolons

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,8 +1,8 @@
 const palindromes = function (words) {
-    words = Array.from(words.toLowerCase()) // converts initial string to an array of lCase letters
+    words = Array.from(words.toLowerCase()); // converts initial string to an array of lCase letters
     .filter(word => word.match(/[a-z]/g)); // converts the array into an array of only a-z characters
     let backwards = words.join(''); // sets backwards to a string, stripping all the commas from the array
-    backwards = Array.from(backwards)
+    backwards = Array.from(backwards);
     backwards = backwards.reverse().join(''); // reverses the array, then converts it to a string stripped of commas
     return words.join('') === backwards;
 }
