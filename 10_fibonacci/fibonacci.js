96e956c67a7e0d4ed38d924c294043b11356523f
logicalmind <ilikelogic@skiff.com>
2022-09-06 15:50:02 -0700

    Refactor solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,14 @@
 const fibonacci = function(fibNum) {
     let arr = [1, 1];
     let seqNum = 0;
-if (fibNum == 1 || fibNum == 2) {
-      return 1;
-    } else if (fibNum < 0) {
-        return "OOPS";
-    } else {
-        fibNum -= 1;
-        for (let i = 1; i < fibNum; i++) {
-            seqNum = (arr[i] + arr[i -1]);
-            arr.push(seqNum);
-    }
-}
-return seqNum;
+if (fibNum == 1 || fibNum == 2) return 1;
+if (fibNum < 0) return "OOPS";
+		fibNum -=1;
+for (let i = 1; i < fibNum; i++) {
+     seqNum = (arr[i] + arr[i -1]);
+     arr.push(seqNum);
+     }
+		 return seqNum;
 };
 
 // Do not edit below this line
