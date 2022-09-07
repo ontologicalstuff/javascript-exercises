f5b7898843483343b870c9a499ff0d39e6fb7c70
logicalmind <ilikelogic@skiff.com>
2022-09-06 19:31:41 -0700

    Refactor solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     let sum = 0;
-    if ( num1 < 0 || num2 < 0 || !(Number.isInteger(num2)) || !(Number.isInteger(num1)) ) return 'ERROR';
+    if ( num1 < 0 || num2 < 0 || !Number.isInteger(num2) || !Number.isInteger(num1) ) return 'ERROR';
     if (num1 < num2) {
         while (num1 <= num2) {
             sum += num1;
